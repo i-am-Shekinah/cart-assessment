c1b9a379d740694b3c95c8712d18ec468f586c39
shekinahcodes <michaelolatunji.dev@gmail.com>
2026-06-10 18:30:31 +0100

    enforce app is connected to the DB before it starts the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,8 +7,18 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+const startServer = async () => {
+    try {
+        console.log('Connecting to the database...');
+        await connectDB();
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    } catch (error) {
+        console.error('Failed to connect to the database:', error);
+        process.exit(1);
+    }
+}
+
+startServer();
