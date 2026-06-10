8ee2db879fe448d44447a6dae0c57a27d776867b
shekinahcodes <michaelolatunji.dev@gmail.com>
2026-06-10 23:49:06 +0100

    update routes, and include global request type

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import authRoutes from '../modules/auth/auth.routes.js';
+import productsRoutes from '../modules/products/products.routes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/products', productsRoutes);
 
 export default router;
